30062545d608457f856895c0bd01a0891440cbb8
apple <apple@apples-MacBook-Pro.local>
2023-06-25 11:42:32 +0700

    fix-register-to-database

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -10,30 +10,22 @@ class AuthController {
     }
     //[POST] /auth/register
     postRegister(req, res, next) {
-        var {
-            fullname, 
-            age, 
-            address, 
-            email, 
-            city, 
-            gender, 
-            username, 
-            password, 
-            confirmPassword} = req.body;
+        var {fullname, age, address, email, city, gender, username, password, confirmPassword} = req.body;
         bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS), function(err, salt) {
-            bcrypt.hash(password, salt, function(err, hashPassword) {
-                const userInfo = {
-                    fullname, 
-                    age, 
-                    address,
-                    email, 
-                    gender, 
-                    username, 
-                    hashPassword, 
+            bcrypt.hash(password, salt, async function(err, hashPassword) {
+                const userInfo = {fullname, age, address, email, gender, username, hashPassword,}
+                var isEmailExisted = await User.findOne({email: email});
+                var isUserNameExisted = await User.findOne({username: username});
+                if(isEmailExisted || isUserNameExisted){
+                    res.render('auth/register',{
+                        pageTitle: 'register',
+                        userInfo: req.body,
+                    });
+                } else {
+                    var user = new User(userInfo);
+                    await user.save();
+                    res.redirect('/auth/login');
                 }
-                var user = new User(userInfo);
-                user.save();
-                res.redirect('/auth/login');
             });
         });
     }
